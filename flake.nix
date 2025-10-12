fbf01dc4f3343311ad99b92e5ec48456e2a580a5
Rohit Singh <rsrohitsingh682@gmail.com>
2025-10-12 17:20:46 +0530

    fix: build

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
 
@@ -24,97 +24,116 @@
     devour-flake.flake = false;
   };
 
-  outputs = inputs:
+  outputs =
+    inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import inputs.systems;
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
       imports = [
         inputs.flake-parts.flakeModules.easyOverlay
         inputs.rust-flake.flakeModules.default
         inputs.rust-flake.flakeModules.nixpkgs
-        inputs.cargo-doc-live.flakeModule
-        inputs.process-compose-flake.flakeModule
+        # inputs.cargo-doc-live.flakeModule
+        # inputs.process-compose-flake.flakeModule
         inputs.pre-commit-hooks-nix.flakeModule
         inputs.treefmt-nix.flakeModule
         inputs.just-flake.flakeModule
       ];
 
-      perSystem = { config, self', pkgs, lib, system, ... }: {
-        rust-project.crane.args = {
-          nativeBuildInputs = with pkgs; with pkgs.darwin.apple_sdk.frameworks; lib.optionals stdenv.isDarwin [
-            Security
-            SystemConfiguration
-          ] ++ [
-            libiconv
-            pkg-config
-          ];
-          buildInputs = lib.optionals pkgs.stdenv.isDarwin
-            (
-              with pkgs.darwin.apple_sdk.frameworks; [
-                IOKit
-                # apple_sdk refers to SDK version 10.12. To compile for `x86_64-darwin` we need 11.0
-                # see: https://github.com/NixOS/nixpkgs/pull/261683#issuecomment-1772935802
-                pkgs.darwin.apple_sdk_11_0.frameworks.CoreFoundation
+      perSystem =
+        {
+          config,
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        {
+          rust-project.crates."nixci".crane.args = {
+            nativeBuildInputs =
+              with pkgs;
+              with pkgs.darwin.apple_sdk.frameworks;
+              lib.optionals stdenv.isDarwin [
+                Security
+                SystemConfiguration
               ]
-            )
-          ++ lib.optionals pkgs.stdenv.isLinux [
-            pkgs.openssl
-          ];
-          DEVOUR_FLAKE = inputs.devour-flake;
-        };
+              ++ [
+                libiconv
+                pkg-config
+              ];
+            buildInputs =
+              lib.optionals pkgs.stdenv.isDarwin (
+                with pkgs.darwin.apple_sdk.frameworks;
+                [
+                  IOKit
+                  CoreFoundation
+                ]
+              )
+              ++ lib.optionals pkgs.stdenv.isLinux [
+                pkgs.openssl
+              ];
+            DEVOUR_FLAKE = inputs.devour-flake;
+          };
 
-        pre-commit = {
-          check.enable = true;
-          settings = {
-            hooks = {
-              treefmt.enable = true;
-              convco.enable = true;
+          pre-commit = {
+            check.enable = true;
+            settings = {
+              hooks = {
+                treefmt.enable = true;
+                convco.enable = true;
+              };
+            };
+          };
+
+          just-flake.features = {
+            treefmt.enable = true;
+            convco.enable = true;
+          };
+
+          # Flake outputs
+          packages.default = self'.packages.nixci.overrideAttrs (oa: {
+            nativeBuildInputs = (oa.nativeBuildInputs or [ ]) ++ [
+              pkgs.installShellFiles
+              pkgs.nix
+            ];
+            postInstall = ''
+              installShellCompletion --cmd nixci \
+                --bash <($out/bin/nixci completion bash) \
+                --zsh <($out/bin/nixci completion zsh) \
+                --fish <($out/bin/nixci completion fish)
+            '';
+          });
+          overlayAttrs.nixci = self'.packages.default;
+
+          devShells.default = pkgs.mkShell {
+            name = "nixci";
+            inputsFrom = [
+              self'.devShells.rust
+              config.treefmt.build.devShell
+              config.just-flake.outputs.devShell
+            ];
+            shellHook = ''
+              export DEVOUR_FLAKE=${inputs.devour-flake}
+            '';
+            packages = [
+              pkgs.cargo-watch
+              #config.process-compose.cargo-doc-live.outputs.package
+              config.pre-commit.settings.tools.convco
+            ];
+          };
+
+          # Add your auto-formatters here.
+          # cf. https://numtide.github.io/treefmt/
+          treefmt.config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              rustfmt.enable = true;
             };
           };
         };
-
-        just-flake.features = {
-          treefmt.enable = true;
-          convco.enable = true;
-        };
-
-        # Flake outputs
-        packages.default = self'.packages.nixci.overrideAttrs (oa: {
-          nativeBuildInputs = (oa.nativeBuildInputs or [ ]) ++ [ pkgs.installShellFiles pkgs.nix ];
-          postInstall = ''
-            installShellCompletion --cmd nixci \
-              --bash <($out/bin/nixci completion bash) \
-              --zsh <($out/bin/nixci completion zsh) \
-              --fish <($out/bin/nixci completion fish)
-          '';
-        });
-        overlayAttrs.nixci = self'.packages.default;
-
-        devShells.default = pkgs.mkShell {
-          name = "nixci";
-          inputsFrom = [
-            self'.devShells.nixci
-            config.treefmt.build.devShell
-            config.just-flake.outputs.devShell
-          ];
-          shellHook = ''
-            export DEVOUR_FLAKE=${inputs.devour-flake}
-          '';
-          packages = [
-            pkgs.cargo-watch
-            config.process-compose.cargo-doc-live.outputs.package
-            config.pre-commit.settings.tools.convco
-          ];
-        };
-
-        # Add your auto-formatters here.
-        # cf. https://numtide.github.io/treefmt/
-        treefmt.config = {
-          projectRootFile = "flake.nix";
-          programs = {
-            nixpkgs-fmt.enable = true;
-            rustfmt.enable = true;
-          };
-        };
-      };
     };
 }
